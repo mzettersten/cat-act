0faa8eab71723dd889d73f3f6368430d7f2ee695
mzettersten <mzettersten@users.noreply.github.com>
2023-01-26 18:02:30 -0500

    working on cleaning up analysis pipeline

--- analysis/v1/helper.R
+++ analysis/v1/helper.R
@@ -5,6 +5,7 @@ convert_array <- function(json_txt, column_name) {
   column_name = rlang::as_name(column_name)
   json_txt %>% 
     gather_array() %>% 
+    unnest(..JSON) %>%
     rename(index=array.index,!!column_name:=..JSON) %>% 
     as_tibble() %>% 
     #pivot_wider(names_prefix = paste0(column_name,"_"), names_from=index,values_from=stims) %>%
