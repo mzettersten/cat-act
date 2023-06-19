2255fc6fa29eaf77b59ece6124c4e6970101775e
mzettersten <mzettersten@users.noreply.github.com>
2023-06-19 13:37:05 -0400

    first commit v4 and v5

---
+++ experiments/v5/js/settings.js
@@ -0,0 +1,72 @@
+// SETTINGS & DEFAULT PARAMETERS
+
+// create default image size
+var grid_image_width = 100;
+var grid_image_height = 100;
+
+var image_path="stims/"
+
+// store names of test phase image files
+var grid_image_names = [
+"stims/ani_c3_sup3.jpg",
+"stims/ani_c3_sup4.jpg",
+"stims/ani_c3_sup5.jpg",
+"stims/ani_c3_sup6.jpg",
+"stims/ani_c3_bas3.jpg",
+"stims/ani_c3_bas4.jpg",
+"stims/ani_c3_sub4.jpg",
+"stims/ani_c3_sub5.jpg",
+"stims/veg_c1_sup3.jpg",
+"stims/veg_c1_sup4.jpg",
+"stims/veg_c1_sup5.jpg",
+"stims/veg_c1_sup6.jpg",
+"stims/veg_c1_bas3.jpg",
+"stims/veg_c1_bas4.jpg",
+"stims/veg_c1_sub4.jpg",
+"stims/veg_c1_sub5.jpg",
+"stims/veh_c2_sup3.jpg",
+"stims/veh_c2_sup4.jpg",
+"stims/veh_c2_sup5.jpg",
+"stims/veh_c2_sup6.jpg",
+"stims/veh_c2_bas3.jpg",
+"stims/veh_c2_bas4.jpg",
+"stims/veh_c2_sub4.jpg",
+"stims/veh_c2_sub5.jpg"];
+
+console.log(grid_image_names)
+
+// store names of sampling phase image files
+var sampling_image_names = [
+"stims/ani_c3_sub4.jpg",
+"stims/ani_c3_sup3.jpg",
+"stims/ani_c3_bas3.jpg",
+"stims/veg_c1_sub4.jpg",
+"stims/veg_c1_sup3.jpg",
+"stims/veg_c1_bas3.jpg",
+"stims/veh_c2_sub4.jpg",
+"stims/veh_c2_sup3.jpg",
+"stims/veh_c2_bas3.jpg"];
+
+var training_image_names = [
+"stims/ani_c3_sub1.jpg",
+"stims/ani_c3_sub2.jpg",
+"stims/ani_c3_sub3.jpg",
+"stims/ani_c3_bas1.jpg",
+"stims/ani_c3_bas2.jpg",
+"stims/ani_c3_sup1.jpg",
+"stims/ani_c3_sup2.jpg",
+"stims/veg_c1_sub1.jpg",
+"stims/veg_c1_sub2.jpg",
+"stims/veg_c1_sub3.jpg",
+"stims/veg_c1_bas1.jpg",
+"stims/veg_c1_bas2.jpg",
+"stims/veg_c1_sup1.jpg",
+"stims/veg_c1_sup2.jpg",
+"stims/veh_c2_sub1.jpg",
+"stims/veh_c2_sub2.jpg",
+"stims/veh_c2_sub3.jpg",
+"stims/veh_c2_bas1.jpg",
+"stims/veh_c2_bas2.jpg",
+"stims/veh_c2_sup1.jpg",
+"stims/veh_c2_sup2.jpg",];
+
